fc48c9ed92890120dcc16f73d2c6c9a1d4fb0197
victornext <victorbrs517@gmail.com>
2025-10-30 23:27:57 -0300

    inicio de tudo

---
+++ src/test/java/com/nextdev/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextdev.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
